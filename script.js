953808bde7b0e4d0966529837a7ab8ba36f80035
daianeklein <daiane.klein22@gmail.com>
2022-03-07 22:44:01 -0300

    add project files

---
+++ script.js
@@ -0,0 +1,8 @@
+var notaPrimeiroBimestre = 9.345
+var notaSegundoBimestre = 8.364
+var notaTerceiroBimestre = 10.293
+var notaQuartoBimestre = 7.8271
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+console.log(notaFinal.toFixed(1))
